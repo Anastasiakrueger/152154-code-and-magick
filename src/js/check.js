32077a5bac32feb7121366dda0f1dd6071dbe50e
anastasiakrueger <anastasiakrueger@yandex.ru>
2016-10-11 23:14:43 +0300

    Внесла изменения в функцию

--- src/js/check.js
+++ src/js/check.js
@@ -7,20 +7,30 @@ function getMessage(a, b) {
       return 'Я никуда не попал';
     }
   }
-  else if (typeof a === 'number') {
+
+  if (typeof a === 'number') {
     return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
   }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+
+    var distancePath = a.reduce(function(prev, current, index) {
+        return prev + current * b[index] }, 0);
+    
+    return 'Я прошёл ' + distancePath + ' метров';
+  }
+
   if (Array.isArray(a)) {
+
        var numberOfSteps = a.reduce(function(sum, current) {
         return sum + current;
-    }, 0);
+    }, 0); 
+
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
-  if (Array.isArray(a) && Array.isArray(b)) {
-     var distancePath = a.reduce(function(prev, current, index) {
-        return prev + current * b[index] }, 0);
-    return 'Я прошёл ' + distancePath + ' метров';
-  } else {
+ else   {
     return 'Переданы некорректные данные';
   }
 }
+
+
