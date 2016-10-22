93729c7af891b28a2787f986ffdb33d664e3c995
Anastasia Murzintseva <anastasiakrueger@yandex.ru>
2016-10-22 20:57:32 +0300

    Первое задание третьего модуля

--- src/js/form.js
+++ src/js/form.js
@@ -3,6 +3,13 @@
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+  var nameUser = document.querySelector('#review-name');
+  var reviewFieldsName = document.querySelector('.review-fields-name');
+  var reviewFieldsText = document.querySelector('.review-fields-text');
+  var reviewFields = document.querySelector('.review-fields');
+  var formOnPage = document.querySelector('.review-form');
+  var button = document.querySelector('.review-form-control');
+  var comment = document.querySelector('#review-text');
 
   var form = {
     onClose: null,
@@ -12,6 +19,8 @@ window.form = (function() {
      */
     open: function(cb) {
       formContainer.classList.remove('invisible');
+      nameUser.setAttribute('required', 'required');
+      form.formIsNotValid();
       cb();
     },
 
@@ -21,9 +30,49 @@ window.form = (function() {
       if (typeof this.onClose === 'function') {
         this.onClose();
       }
+    },
+
+    formIsNotValid: function() {
+      button.disabled = true;
+      reviewFields.classList.remove('invisible');
+    },
+
+    formIsValid: function() {
+      button.disabled = false;
+      reviewFields.classList.add('invisible');
+    },
+
+    inspectionStars: function() {
+      var radio = formOnPage.elements['review-mark'].value;
+      console.log(typeof radio);
+      if (radio < 3) {
+        comment.setAttribute('required', 'required');
+      } else {
+        comment.removeAttribute('required');
+      }
+    },
+
+    validateForm: function() {
+      form.inspectionStars();
+      if (nameUser.value.length === 0) {
+        form.formIsNotValid();
+        reviewFieldsName.classList.remove('invisible');
+      } else {
+        form.formIsValid();
+        reviewFieldsName.classList.add('invisible');
+      }
+      if (comment.required && comment.value.length === 0 ) {
+        form.formIsNotValid();
+        reviewFieldsText.classList.remove('invisible');
+      } else {
+        form.formIsValid();
+        reviewFieldsText.classList.add('invisible');
+      }
     }
   };
 
+  formOnPage.onchange = form.validateForm;
+  formOnPage.oninput = form.validateForm;
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
@@ -31,4 +80,5 @@ window.form = (function() {
   };
 
   return form;
+
 })();
